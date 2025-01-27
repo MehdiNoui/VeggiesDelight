c2f363fe67786aba84c92b25d82c9a6531adf3d4
Mehdi <mehdinoui2004@gmail.com>
2025-01-27 15:27:30 +0000

    .

--- src/main/java/net/mehdinoui/veggiesdelight/world/VillageStructures.java
+++ src/main/java/net/mehdinoui/veggiesdelight/world/VillageStructures.java
@@ -23,7 +23,7 @@ public class VillageStructures
         Registry<StructureProcessorList> processorLists = event.getServer().registryAccess().registry(Registries.PROCESSOR_LIST).get();
 
         VillageStructures.addBuildingToPool(templatePools, processorLists, new ResourceLocation("minecraft:village/plains/houses"), "veggiesdelight:village/houses/plains_depot_house", 4);
-        VillageStructures.addBuildingToPool(templatePools, processorLists, new ResourceLocation("minecraft:village/snowy/houses"), "veggiesdelight:village/houses/snowy_depot_house", 3);
+        VillageStructures.addBuildingToPool(templatePools, processorLists, new ResourceLocation("minecraft:village/snowy/houses"), "veggiesdelight:village/houses/snowy_depot_house", 5);
         VillageStructures.addBuildingToPool(templatePools, processorLists, new ResourceLocation("minecraft:village/taiga/houses"), "veggiesdelight:village/houses/taiga_depot_house", 5);
         VillageStructures.addBuildingToPool(templatePools, processorLists, new ResourceLocation("minecraft:village/savanna/houses"), "veggiesdelight:village/houses/savanna_depot_house", 4);
         VillageStructures.addBuildingToPool(templatePools, processorLists, new ResourceLocation("minecraft:village/desert/houses"), "veggiesdelight:village/houses/desert_depot_house", 4);

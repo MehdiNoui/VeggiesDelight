7d695b1d7fd1060f47bb9d8b74f0752d2f9f8f12
Mehdi <mehdinoui2004@gmail.com>
2025-02-07 22:45:25 +0000

    version 1.6.2

--- src/main/java/net/mehdinoui/veggiesdelight/block/custom/DandelionBlock.java
+++ src/main/java/net/mehdinoui/veggiesdelight/block/custom/DandelionBlock.java
@@ -1,7 +1,6 @@
 package net.mehdinoui.veggiesdelight.block.custom;
 
 import net.minecraft.core.BlockPos;
-import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.effect.MobEffects;

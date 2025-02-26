e8ae94c697791f59cb2af7d90d32333dc5f4910e
Mehdi <mehdinoui2004@gmail.com>
2025-02-26 20:47:51 +0000

    Updated the mature dandelion class

--- src/main/java/net/mehdinoui/veggiesdelight/block/custom/DandelionBlock.java
+++ src/main/java/net/mehdinoui/veggiesdelight/block/custom/DandelionBlock.java
@@ -1,38 +1,35 @@
 package net.mehdinoui.veggiesdelight.block.custom;
 
+import net.mehdinoui.veggiesdelight.block.ModBlocks;
 import net.minecraft.core.BlockPos;
-import net.minecraft.world.InteractionHand;
-import net.minecraft.world.InteractionResult;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.util.RandomSource;
 import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.level.LevelReader;
+import net.minecraft.world.level.block.BonemealableBlock;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Items;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.FlowerBlock;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.phys.BlockHitResult;
-import net.minecraftforge.common.util.FakePlayer;
 
-public class DandelionBlock extends FlowerBlock {
+public class DandelionBlock extends FlowerBlock implements BonemealableBlock{
 
     public DandelionBlock(Properties properties) {
         super(() -> MobEffects.LUCK, 5, properties);
     }
 
     @Override
-    public InteractionResult use(BlockState state, Level level, BlockPos pos,
-                                 net.minecraft.world.entity.player.Player player,
-                                 InteractionHand hand, BlockHitResult hit) {
-        if (!level.isClientSide && player.getItemInHand(hand).getItem() == Items.BONE_MEAL) {
-            if (level.getRandom().nextFloat() < 0.5f) {
-                popResource(level, pos, new ItemStack(this));
-            }
-            if (!(player instanceof FakePlayer) && !player.isCreative()) {
-                player.getItemInHand(hand).shrink(1);
-            }
+    public boolean isValidBonemealTarget(LevelReader levelReader, BlockPos blockPos, BlockState blockState, boolean b) {
+        return true;
+    }
 
-            return InteractionResult.SUCCESS;
-        }
+    @Override
+    public boolean isBonemealSuccess(Level level, RandomSource randomSource, BlockPos blockPos, BlockState blockState) {
+        return true;
+    }
 
-        return InteractionResult.PASS;
+    @Override
+    public void performBonemeal(ServerLevel serverLevel, RandomSource randomSource, BlockPos blockPos, BlockState blockState) {
+        popResource(serverLevel, blockPos, new ItemStack(ModBlocks.MATURE_DANDELION.get()));
     }
 }

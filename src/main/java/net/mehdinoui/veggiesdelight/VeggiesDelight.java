cd957c6405ba896ff67a58cbd457c2044baf4c03
Mehdi Noui <mehdinoui2004@gmail.com>
2025-08-02 21:06:14 +0100

    Updated pig/board(Naturalist) food diet

--- src/main/java/net/mehdinoui/veggiesdelight/VeggiesDelight.java
+++ src/main/java/net/mehdinoui/veggiesdelight/VeggiesDelight.java
@@ -64,57 +64,61 @@ public class VeggiesDelight
         @SubscribeEvent
         public static void onCommonSetup(FMLCommonSetupEvent event) {
             event.enqueueWork(() -> {
-                    //10%
-                    ComposterBlock.COMPOSTABLES.put(ModItems.GARLIC_CLOVE.get(),0.1F);
+                //10%
+                ComposterBlock.COMPOSTABLES.put(ModItems.GARLIC_CLOVE.get(),0.1F);
+                ComposterBlock.COMPOSTABLES.put(ModItems.GARLIC_CLOVE.get(),0.1F);
 
-                    //30%
-                    ComposterBlock.COMPOSTABLES.put(ModItems.BELLPEPPER_SEEDS.get(), 0.3F);
-                    ComposterBlock.COMPOSTABLES.put(ModItems.BROCCOLI_SEEDS.get(), 0.3F);
-                    ComposterBlock.COMPOSTABLES.put(ModItems.CAULIFLOWER_SEEDS.get(), 0.3F);
-                    ComposterBlock.COMPOSTABLES.put(ModItems.CAULIFLOWER_FLORET.get(), 0.3F);
-                    ComposterBlock.COMPOSTABLES.put(ModItems.DANDELION_LEAF.get(),0.3F);
-                    ComposterBlock.COMPOSTABLES.put(ModItems.TURNIP_SEEDS.get(), 0.3F);
-                    ComposterBlock.COMPOSTABLES.put(ModItems.ZUCCHINI_SEEDS.get(), 0.3F);
+                //30%
+                ComposterBlock.COMPOSTABLES.put(ModItems.BELLPEPPER_SEEDS.get(), 0.3F);
+                ComposterBlock.COMPOSTABLES.put(ModItems.BROCCOLI_SEEDS.get(), 0.3F);
+                ComposterBlock.COMPOSTABLES.put(ModItems.CAULIFLOWER_SEEDS.get(), 0.3F);
+                ComposterBlock.COMPOSTABLES.put(ModItems.CAULIFLOWER_FLORET.get(), 0.3F);
+                ComposterBlock.COMPOSTABLES.put(ModItems.DANDELION_LEAF.get(),0.3F);
+                ComposterBlock.COMPOSTABLES.put(ModItems.TURNIP_SEEDS.get(), 0.3F);
+                ComposterBlock.COMPOSTABLES.put(ModItems.ZUCCHINI_SEEDS.get(), 0.3F);
 
-                    //65%
-                    ComposterBlock.COMPOSTABLES.put(ModItems.BELLPEPPER.get(), 0.65F);
-                    ComposterBlock.COMPOSTABLES.put(ModItems.BROCCOLI.get(), 0.65F);
-                    ComposterBlock.COMPOSTABLES.put(ModItems.CAULIFLOWER.get(), 0.65F);
-                    ComposterBlock.COMPOSTABLES.put(ModItems.GARLIC.get(),0.65F);
-                    ComposterBlock.COMPOSTABLES.put(ModItems.SWEET_POTATO.get(), 0.65F);
-                    ComposterBlock.COMPOSTABLES.put(ModItems.TURNIP.get(), 0.65F);
-                    ComposterBlock.COMPOSTABLES.put(ModItems.ZUCCHINI.get(), 0.65F);
-                    ComposterBlock.COMPOSTABLES.put(ModBlocks.WILD_BELLPEPPERS.get().asItem(), 0.65F);
-                    ComposterBlock.COMPOSTABLES.put(ModBlocks.WILD_BROCCOLI.get().asItem(), 0.65F);
-                    ComposterBlock.COMPOSTABLES.put(ModBlocks.WILD_CAULIFLOWERS.get().asItem(), 0.65F);
-                    ComposterBlock.COMPOSTABLES.put(ModBlocks.WILD_GARLIC.get().asItem(), 0.65F);
-                    ComposterBlock.COMPOSTABLES.put(ModBlocks.WILD_SWEET_POTATOES.get().asItem(), 0.65F);
-                    ComposterBlock.COMPOSTABLES.put(ModBlocks.WILD_TURNIPS.get().asItem(), 0.65F);
-                    ComposterBlock.COMPOSTABLES.put(ModBlocks.WILD_ZUCCHINIS.get().asItem(), 0.65F);
-                    ComposterBlock.COMPOSTABLES.put(ModBlocks.MATURE_DANDELION.get().asItem(), 0.65F);
+                //65%
+                ComposterBlock.COMPOSTABLES.put(ModItems.BELLPEPPER.get(), 0.65F);
+                ComposterBlock.COMPOSTABLES.put(ModItems.BROCCOLI.get(), 0.65F);
+                ComposterBlock.COMPOSTABLES.put(ModItems.CAULIFLOWER.get(), 0.65F);
+                ComposterBlock.COMPOSTABLES.put(ModItems.GARLIC.get(),0.65F);
+                ComposterBlock.COMPOSTABLES.put(ModItems.SWEET_POTATO.get(), 0.65F);
+                ComposterBlock.COMPOSTABLES.put(ModItems.TURNIP.get(), 0.65F);
+                ComposterBlock.COMPOSTABLES.put(ModItems.ZUCCHINI.get(), 0.65F);
+                ComposterBlock.COMPOSTABLES.put(ModBlocks.WILD_BELLPEPPERS.get().asItem(), 0.65F);
+                ComposterBlock.COMPOSTABLES.put(ModBlocks.WILD_BROCCOLI.get().asItem(), 0.65F);
+                ComposterBlock.COMPOSTABLES.put(ModBlocks.WILD_CAULIFLOWERS.get().asItem(), 0.65F);
+                ComposterBlock.COMPOSTABLES.put(ModBlocks.WILD_GARLIC.get().asItem(), 0.65F);
+                ComposterBlock.COMPOSTABLES.put(ModBlocks.WILD_SWEET_POTATOES.get().asItem(), 0.65F);
+                ComposterBlock.COMPOSTABLES.put(ModBlocks.WILD_TURNIPS.get().asItem(), 0.65F);
+                ComposterBlock.COMPOSTABLES.put(ModBlocks.WILD_ZUCCHINIS.get().asItem(), 0.65F);
+                ComposterBlock.COMPOSTABLES.put(ModBlocks.MATURE_DANDELION.get().asItem(), 0.65F);
 
-                    //85%
-                    ComposterBlock.COMPOSTABLES.put(ModItems.BEETROOT_BROWNIE.get(), 0.85F);
-                    ComposterBlock.COMPOSTABLES.put(ModItems.VEGAN_PIZZA_SLICE.get(), 0.85F);
-                    ComposterBlock.COMPOSTABLES.put(ModItems.SWEET_POTATO_PIE_SLICE.get(), 0.85F);
-                    ComposterBlock.COMPOSTABLES.put(ModItems.CARROT_CAKE_SLICE.get(), 0.85F);
-                    ComposterBlock.COMPOSTABLES.put(ModItems.POTATO_NOODLE.get(), 0.85F);
-                    ComposterBlock.COMPOSTABLES.put(ModItems.CAULIFLOWER_PATTY.get(), 0.85F);
-                    ComposterBlock.COMPOSTABLES.put(ModItems.UNCOOKED_MHADJEB.get(), 0.85F);
-                    ComposterBlock.COMPOSTABLES.put(ModItems.SWEET_POTATO_DOUGH.get(), 0.85F);
-                    ComposterBlock.COMPOSTABLES.put(ModItems.SWEET_POTATO_CUPCAKE.get(), 0.85F);
+                //85%
+                ComposterBlock.COMPOSTABLES.put(ModItems.BEETROOT_BROWNIE.get(), 0.85F);
+                ComposterBlock.COMPOSTABLES.put(ModItems.VEGAN_PIZZA_SLICE.get(), 0.85F);
+                ComposterBlock.COMPOSTABLES.put(ModItems.SWEET_POTATO_PIE_SLICE.get(), 0.85F);
+                ComposterBlock.COMPOSTABLES.put(ModItems.CARROT_CAKE_SLICE.get(), 0.85F);
+                ComposterBlock.COMPOSTABLES.put(ModItems.POTATO_NOODLE.get(), 0.85F);
+                ComposterBlock.COMPOSTABLES.put(ModItems.CAULIFLOWER_PATTY.get(), 0.85F);
+                ComposterBlock.COMPOSTABLES.put(ModItems.UNCOOKED_MHADJEB.get(), 0.85F);
+                ComposterBlock.COMPOSTABLES.put(ModItems.SWEET_POTATO_DOUGH.get(), 0.85F);
+                ComposterBlock.COMPOSTABLES.put(ModItems.SWEET_POTATO_CUPCAKE.get(), 0.85F);
 
-                    //100%
-                    ComposterBlock.COMPOSTABLES.put(ModItems.VEGAN_PIZZA.get(), 1F);
-                    ComposterBlock.COMPOSTABLES.put(ModItems.SWEET_POTATO_PIE.get(), 1F);
-                    ComposterBlock.COMPOSTABLES.put(ModItems.BEETROOT_BROWNIE_TRAY.get(), 1F);
-                    ComposterBlock.COMPOSTABLES.put(ModItems.CARROT_CAKE.get(), 1F);
+                //100%
+                ComposterBlock.COMPOSTABLES.put(ModItems.VEGAN_PIZZA.get(), 1F);
+                ComposterBlock.COMPOSTABLES.put(ModItems.SWEET_POTATO_PIE.get(), 1F);
+                ComposterBlock.COMPOSTABLES.put(ModItems.BEETROOT_BROWNIE_TRAY.get(), 1F);
+                ComposterBlock.COMPOSTABLES.put(ModItems.CARROT_CAKE.get(), 1F);
 
                 Ingredient newPigFood = Ingredient.of(
                         ModItems.BELLPEPPER.get(),
+                        ModItems.CAULIFLOWER.get(),
+                        ModItems.CAULIFLOWER_FLORET.get(),
                         ModItems.SWEET_POTATO.get(),
                         ModItems.TURNIP.get(),
-                        ModItems.ZUCCHINI.get());
+                        ModItems.ZUCCHINI.get(),
+                        ModItems.ZUCCHINI_SLICE.get());
                 Pig.FOOD_ITEMS = new CompoundIngredient(Arrays.asList(Pig.FOOD_ITEMS, newPigFood)) {};
 
                 Ingredient newChickenFood = Ingredient.of(

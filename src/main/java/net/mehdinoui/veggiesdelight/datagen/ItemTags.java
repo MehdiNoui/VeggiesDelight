2a4c870f586672f16ded02834160d1af631ecb7a
Mehdi Noui <mehdinoui2004@gmail.com>
2025-09-29 00:41:06 +0100

    add item and block tags data generator

---
+++ src/main/java/net/mehdinoui/veggiesdelight/datagen/ItemTags.java
@@ -0,0 +1,255 @@
+package net.mehdinoui.veggiesdelight.datagen;
+
+import net.mehdinoui.veggiesdelight.VeggiesDelight;
+import net.mehdinoui.veggiesdelight.registry.ModItems;
+import net.mehdinoui.veggiesdelight.tag.VDCommonTags;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.data.PackOutput;
+import net.minecraft.data.tags.ItemTagsProvider;
+import net.minecraft.data.tags.TagsProvider;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.level.block.Block;
+import net.neoforged.neoforge.common.Tags;
+import net.neoforged.neoforge.common.data.ExistingFileHelper;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import vectorwing.farmersdelight.common.tag.CommonTags;
+import vectorwing.farmersdelight.common.tag.ModTags;
+
+import java.util.concurrent.CompletableFuture;
+
+public class ItemTags extends ItemTagsProvider {
+    public ItemTags(PackOutput output, CompletableFuture<HolderLookup.Provider> provider, CompletableFuture<TagsProvider.TagLookup<Block>> blockTagProvider, @Nullable ExistingFileHelper existingFileHelper) {
+        super(output, provider, blockTagProvider, VeggiesDelight.MOD_ID, existingFileHelper);
+    }
+    @Override
+    protected void addTags(HolderLookup.@NotNull Provider provider) {
+        this.registerCommonTags();
+        this.registerMinecraftTags();
+        this.registerFarmersDelightTags();
+        this.registerNeoforgeTags();
+    }
+    protected void registerCommonTags() {
+        tag(VDCommonTags.CROPS_BELLPEPPER).add(ModItems.BELLPEPPER.get());
+        tag(VDCommonTags.CROPS_BROCCOLI).add(ModItems.BROCCOLI.get());
+        tag(VDCommonTags.CROPS_CAULIFLOWER).add(ModItems.CAULIFLOWER.get());
+        tag(VDCommonTags.CROPS_CAULIFLOWER).add(ModItems.CAULIFLOWER_FLORET.get());
+        tag(VDCommonTags.CROPS_GARLIC).add(ModItems.GARLIC.get());
+        tag(VDCommonTags.CROPS_GARLIC).add(ModItems.GARLIC_CLOVE.get());
+        tag(VDCommonTags.CROPS_SWEET_POTATO).add(ModItems.SWEET_POTATO.get());
+        tag(VDCommonTags.CROPS_TURNIP).add(ModItems.TURNIP.get());
+        tag(VDCommonTags.CROPS_ZUCCHINI).add(ModItems.ZUCCHINI.get());
+        tag(VDCommonTags.CROPS_ZUCCHINI).add(ModItems.ZUCCHINI_SLICE.get());
+
+        tag(VDCommonTags.FOODS_BELLPEPPER).add(ModItems.BELLPEPPER.get());
+        tag(VDCommonTags.FOODS_BROCCOLI).add(ModItems.BROCCOLI.get());
+        tag(VDCommonTags.FOODS_CAULIFLOWER).add(ModItems.CAULIFLOWER.get());
+        tag(VDCommonTags.FOODS_CAULIFLOWER).add(ModItems.CAULIFLOWER_FLORET.get());
+        tag(VDCommonTags.FOODS_GARLIC).add(ModItems.GARLIC.get());
+        tag(VDCommonTags.FOODS_GARLIC).add(ModItems.GARLIC_CLOVE.get());
+        tag(VDCommonTags.FOODS_SWEET_POTATO).add(ModItems.SWEET_POTATO.get());
+        tag(VDCommonTags.FOODS_TURNIP).add(ModItems.TURNIP.get());
+        tag(VDCommonTags.FOODS_ZUCCHINI).add(ModItems.ZUCCHINI.get());
+        tag(VDCommonTags.FOODS_ZUCCHINI).add(ModItems.ZUCCHINI_SLICE.get());
+
+        tag(CommonTags.FOODS_DOUGH).add(ModItems.SWEET_POTATO_DOUGH.get());
+
+        tag(CommonTags.FOODS_LEAFY_GREEN).add(ModItems.DANDELION_LEAF.get());
+        tag(VDCommonTags.FOODS_POTATOES).add(
+                Items.POTATO,
+                ModItems.SWEET_POTATO.get()
+        );
+        /* Legacy Tags
+        tag(CommonTags.BREAD).addTag(VDCommonTags.BREAD_CAULIFLOWER);
+        tag(VDCommonTags.BREAD_CAULIFLOWER).add(ModItems.CAULIFLOWER_BREAD.get());
+
+        tag(CommonTags.SEEDS).addTag(VDCommonTags.SEEDS_BELLPEPPER);
+        tag(VDCommonTags.SEEDS_BELLPEPPER).add(ModItems.BELLPEPPER_SEEDS.get());
+        tag(CommonTags.SEEDS).addTag(VDCommonTags.SEEDS_BROCCOLI);
+        tag(VDCommonTags.SEEDS_BROCCOLI).add(ModItems.BROCCOLI_SEEDS.get());
+        tag(CommonTags.SEEDS).addTag(VDCommonTags.SEEDS_CAULIFLOWER);
+        tag(VDCommonTags.SEEDS_CAULIFLOWER).add(ModItems.CAULIFLOWER_SEEDS.get());
+        tag(CommonTags.SEEDS).addTag(VDCommonTags.SEEDS_TURNIP);
+        tag(VDCommonTags.SEEDS_TURNIP).add(ModItems.TURNIP_SEEDS.get());
+        tag(CommonTags.SEEDS).addTag(VDCommonTags.SEEDS_ZUCCHINI);
+        tag(VDCommonTags.SEEDS_ZUCCHINI).add(ModItems.ZUCCHINI_SEEDS.get());
+
+        tag(CommonTags.VEGETABLES).addTag(VDCommonTags.VEGETABLES_BELLPEPPER);
+        tag(VDCommonTags.VEGETABLES_BELLPEPPER).add(ModItems.BELLPEPPER.get());
+        tag(CommonTags.VEGETABLES).addTag(VDCommonTags.VEGETABLES_BROCCOLI);
+        tag(VDCommonTags.VEGETABLES_BROCCOLI).add(ModItems.BROCCOLI.get());
+        tag(CommonTags.VEGETABLES).addTag(VDCommonTags.VEGETABLES_CAULIFLOWER);
+        tag(VDCommonTags.VEGETABLES_CAULIFLOWER).add(ModItems.CAULIFLOWER.get());
+        tag(VDCommonTags.VEGETABLES_CAULIFLOWER).add(ModItems.CAULIFLOWER_FLORET.get());
+        tag(CommonTags.VEGETABLES).addTag(VDCommonTags.VEGETABLES_GARLIC);
+        tag(VDCommonTags.VEGETABLES_GARLIC).add(ModItems.GARLIC.get());
+        tag(VDCommonTags.VEGETABLES_GARLIC).add(ModItems.GARLIC_CLOVE.get());
+        tag(CommonTags.VEGETABLES).addTag(VDCommonTags.VEGETABLES_SWEET_POTATO);
+        tag(VDCommonTags.VEGETABLES_SWEET_POTATO).add(ModItems.SWEET_POTATO.get());
+        tag(CommonTags.VEGETABLES).addTag(VDCommonTags.VEGETABLES_TURNIP);
+        tag(VDCommonTags.VEGETABLES_TURNIP).add(ModItems.TURNIP.get());
+        tag(CommonTags.VEGETABLES).addTag(VDCommonTags.VEGETABLES_ZUCCHINI);
+        tag(VDCommonTags.VEGETABLES_ZUCCHINI).add(ModItems.ZUCCHINI.get());
+        tag(VDCommonTags.VEGETABLES_ZUCCHINI).add(ModItems.ZUCCHINI_SLICE.get());
+        */
+    }
+    protected void registerMinecraftTags() {
+        // Animals food
+        tag(net.minecraft.tags.ItemTags.ARMADILLO_FOOD).add(
+                ModItems.DANDELION_LEAF.get()
+        );
+        tag(net.minecraft.tags.ItemTags.CAMEL_FOOD).add(
+                ModItems.DANDELION_LEAF.get()
+        );
+        tag(net.minecraft.tags.ItemTags.CHICKEN_FOOD).add(
+                ModItems.BELLPEPPER_SEEDS.get(),
+                ModItems.BROCCOLI_SEEDS.get(),
+                ModItems.CAULIFLOWER_SEEDS.get(),
+                ModItems.TURNIP_SEEDS.get(),
+                ModItems.ZUCCHINI_SEEDS.get()
+        );
+        tag(net.minecraft.tags.ItemTags.COW_FOOD).add(
+                ModItems.DANDELION_LEAF.get(),
+                ModItems.BROCCOLI.get(),
+                ModItems.CAULIFLOWER.get(),
+                ModItems.CAULIFLOWER_FLORET.get()
+        );
+        tag(net.minecraft.tags.ItemTags.GOAT_FOOD).add(
+                ModItems.DANDELION_LEAF.get(),
+                ModItems.BROCCOLI.get(),
+                ModItems.CAULIFLOWER.get()
+        );
+        tag(net.minecraft.tags.ItemTags.HORSE_FOOD).add(
+                ModItems.DANDELION_LEAF.get(),
+                ModItems.BROCCOLI.get(),
+                ModItems.CAULIFLOWER.get(),
+                ModItems.SWEET_POTATO.get(),
+                ModItems.TURNIP.get(),
+                ModItems.ZUCCHINI.get()
+        );
+        tag(net.minecraft.tags.ItemTags.LLAMA_FOOD).add(
+                ModItems.DANDELION_LEAF.get()
+        );
+        tag(net.minecraft.tags.ItemTags.PARROT_FOOD).add(
+                ModItems.BROCCOLI_SEEDS.get(),
+                ModItems.CAULIFLOWER_SEEDS.get(),
+                ModItems.TURNIP_SEEDS.get(),
+                ModItems.ZUCCHINI_SEEDS.get()
+        );
+        tag(net.minecraft.tags.ItemTags.PARROT_POISONOUS_FOOD).add(
+                ModItems.BELLPEPPER_SEEDS.get()
+        );
+        tag(net.minecraft.tags.ItemTags.PIG_FOOD).add(
+                ModItems.BELLPEPPER.get(),
+                ModItems.BROCCOLI.get(),
+                ModItems.CAULIFLOWER.get(),
+                ModItems.CAULIFLOWER_FLORET.get(),
+                ModItems.SWEET_POTATO.get(),
+                ModItems.TURNIP.get(),
+                ModItems.ZUCCHINI.get(),
+                ModItems.ZUCCHINI_SLICE.get(),
+                ModItems.DANDELION_LEAF.get()
+        );
+        tag(net.minecraft.tags.ItemTags.RABBIT_FOOD).add(
+                ModItems.DANDELION_LEAF.get(),
+                ModItems.BROCCOLI.get(),
+                ModItems.CAULIFLOWER_FLORET.get(),
+                ModItems.ZUCCHINI_SLICE.get()
+        );
+        tag(net.minecraft.tags.ItemTags.SHEEP_FOOD).add(
+                ModItems.DANDELION_LEAF.get(),
+                ModItems.BROCCOLI.get(),
+                ModItems.CAULIFLOWER.get()
+        );
+        // Villager
+        tag(net.minecraft.tags.ItemTags.VILLAGER_PLANTABLE_SEEDS).add(
+                ModItems.BELLPEPPER_SEEDS.get(),
+                ModItems.BROCCOLI_SEEDS.get(),
+                ModItems.CAULIFLOWER_SEEDS.get(),
+                ModItems.GARLIC_CLOVE.get(),
+                ModItems.SWEET_POTATO.get(),
+                ModItems.TURNIP_SEEDS.get(),
+                ModItems.ZUCCHINI_SEEDS.get()
+        );
+    }
+    protected void registerFarmersDelightTags(){
+        tag(ModTags.DRINKS).add(
+                ModItems.CARROT_JUICE.get(),
+                ModItems.DANDELION_JUICE.get(),
+                ModItems.TURNIP_WATER.get()
+        );
+        tag(ModTags.MEALS).add(
+                // Bell pepper
+                ModItems.CACCIATORE.get(),
+                ModItems.SHAKSHOUKA.get(),
+                ModItems.STUFFED_BELLPEPPERS.get(),
+                // Broccoli
+                ModItems.BROCCOLI_SOUP.get(),
+                ModItems.PASTA_WITH_BROCCOLI.get(),
+                // Cauliflower
+                ModItems.CAULIFLOWER_KUKU.get(),
+                ModItems.CAULIFLOWER_SOUP.get(),
+                // Dandelion
+                ModItems.DANDELION_AND_EGGS.get(),
+                // Garlic
+                ModItems.GARLIC_BAKED_COD.get(),
+                ModItems.GARLIC_CHICKEN_STEW.get(),
+                ModItems.GARLIC_RICE_WITH_CAULIFLOWER.get(),
+                // Sweet Potato
+                ModItems.MASHED_POTATOES.get(),
+                ModItems.POTATO_NOODLES.get(),
+                // Turnip
+                ModItems.TURNIP_BEEF_STEW.get(),
+                ModItems.TURNIP_CAKE.get(),
+                // Zucchini
+                ModItems.STUFFED_ZUCCHINIS.get()
+        );
+    }
+    protected void registerNeoforgeTags() {
+        tag(Tags.Items.FOODS_BREAD).add(ModItems.CAULIFLOWER_BREAD.get());
+
+        tag(Tags.Items.FOODS_VEGETABLE).add(ModItems.BELLPEPPER.get());
+        tag(Tags.Items.FOODS_VEGETABLE).add(ModItems.BROCCOLI.get());
+        tag(Tags.Items.FOODS_VEGETABLE).add(ModItems.CAULIFLOWER.get());
+        tag(Tags.Items.FOODS_VEGETABLE).add(ModItems.CAULIFLOWER_FLORET.get());
+        tag(Tags.Items.FOODS_VEGETABLE).add(ModItems.GARLIC.get());
+        tag(Tags.Items.FOODS_VEGETABLE).add(ModItems.GARLIC_CLOVE.get());
+        tag(Tags.Items.FOODS_VEGETABLE).add(ModItems.SWEET_POTATO.get());
+        tag(Tags.Items.FOODS_VEGETABLE).add(ModItems.TURNIP.get());
+        tag(Tags.Items.FOODS_VEGETABLE).add(ModItems.ZUCCHINI.get());
+        tag(Tags.Items.FOODS_VEGETABLE).add(ModItems.ZUCCHINI_SLICE.get());
+
+        tag(Tags.Items.FOODS_EDIBLE_WHEN_PLACED).add(ModItems.BEETROOT_BROWNIE_TRAY.get());
+        tag(Tags.Items.FOODS_EDIBLE_WHEN_PLACED).add(ModItems.CARROT_CAKE.get());
+        tag(Tags.Items.FOODS_EDIBLE_WHEN_PLACED).add(ModItems.SWEET_POTATO_PIE.get());
+        tag(Tags.Items.FOODS_EDIBLE_WHEN_PLACED).add(ModItems.VEGAN_PIZZA.get());
+
+        tag(Tags.Items.FOODS_PIE).add(ModItems.BEETROOT_BROWNIE_TRAY.get());
+        tag(Tags.Items.FOODS_PIE).add(ModItems.SWEET_POTATO_PIE.get());
+
+        tag(Tags.Items.SEEDS).add(
+                ModItems.BELLPEPPER_SEEDS.get(),
+                ModItems.BROCCOLI_SEEDS.get(),
+                ModItems.CAULIFLOWER_SEEDS.get(),
+                ModItems.TURNIP_SEEDS.get(),
+                ModItems.ZUCCHINI_SEEDS.get()
+        );
+
+        tag(Tags.Items.CROPS).add(
+                ModItems.BELLPEPPER.get(),
+                ModItems.BROCCOLI.get(),
+                ModItems.CAULIFLOWER.get(),
+                ModItems.CAULIFLOWER_FLORET.get(),
+                ModItems.SWEET_POTATO.get(),
+                ModItems.TURNIP.get(),
+                ModItems.ZUCCHINI.get(),
+                ModItems.ZUCCHINI_SLICE.get()
+        );
+
+        tag(Tags.Items.DRINKS_JUICE).add(
+                ModItems.CARROT_JUICE.get(),
+                ModItems.DANDELION_JUICE.get(),
+                ModItems.TURNIP_WATER.get()
+        );
+    }
+}

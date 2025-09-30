83f967711699c2d4b0093fd71ae0fd3431200fa4
MehdiNoui <mehdinoui2004@gmail.com>
2025-09-30 18:21:48 +0100

    add vd's items to wanted villager items

--- src/main/java/net/mehdinoui/veggiesdelight/event/ModAnimalsEvents.java
+++ src/main/java/net/mehdinoui/veggiesdelight/event/ModAnimalsEvents.java
@@ -1,8 +1,12 @@
 package net.mehdinoui.veggiesdelight.event;
 
+import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Sets;
 import net.mehdinoui.veggiesdelight.registry.ModItems;
 import net.minecraft.world.entity.animal.Chicken;
 import net.minecraft.world.entity.animal.Pig;
+import net.minecraft.world.entity.npc.Villager;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraftforge.common.crafting.CompoundIngredient;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -10,6 +14,7 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 
 import java.util.Arrays;
+import java.util.Set;
 
 import static net.mehdinoui.veggiesdelight.VeggiesDelight.MOD_ID;
 
@@ -37,5 +42,25 @@ public class ModAnimalsEvents {
             );
             Chicken.FOOD_ITEMS = new CompoundIngredient(Arrays.asList(Chicken.FOOD_ITEMS, newChickenFood)) {};
         });
+
+        Set<Item> newWantedItems = Sets.newHashSet(
+                ModItems.CAULIFLOWER_BREAD.get(),
+
+                ModItems.BELLPEPPER.get(),
+                ModItems.BELLPEPPER_SEEDS.get(),
+                ModItems.BROCCOLI.get(),
+                ModItems.BROCCOLI_SEEDS.get(),
+                ModItems.CAULIFLOWER.get(),
+                ModItems.CAULIFLOWER_SEEDS.get(),
+                ModItems.GARLIC.get(),
+                ModItems.GARLIC_CLOVE.get(),
+                ModItems.SWEET_POTATO.get(),
+                ModItems.TURNIP.get(),
+                ModItems.TURNIP_SEEDS.get(),
+                ModItems.ZUCCHINI.get(),
+                ModItems.ZUCCHINI_SLICE.get());
+
+        newWantedItems.addAll(Villager.WANTED_ITEMS);
+        Villager.WANTED_ITEMS = ImmutableSet.copyOf(newWantedItems);
     }
 }

41574c01d90fed52dbbe29d39b03182428c780f8
MehdiNoui <mehdinoui2004@gmail.com>
2025-11-01 15:18:54 +0100

    add nature's spirit compat

--- src/main/java/net/mehdinoui/veggiesdelight/world/VillageStructures.java
+++ src/main/java/net/mehdinoui/veggiesdelight/world/VillageStructures.java
@@ -17,27 +17,72 @@ import net.minecraftforge.fml.ModList;
 import java.util.ArrayList;
 import java.util.List;
 
-
 public class VillageStructures
 {
     public static void addNewVillageBuilding(final ServerAboutToStartEvent event) {
         if (!Configuration.GENERATE_VILLAGE_STRUCTURES.get()) {
             return;
         }
-
         Registry<StructureTemplatePool> templatePools = event.getServer().registryAccess().registry(Registries.TEMPLATE_POOL).get();
         Registry<StructureProcessorList> processorLists = event.getServer().registryAccess().registry(Registries.PROCESSOR_LIST).get();
 
-        VillageStructures.addBuildingToPool(templatePools, processorLists, new ResourceLocation("minecraft:village/plains/houses"), "veggiesdelight:village/houses/plains_depot_house", 4);
-        VillageStructures.addBuildingToPool(templatePools, processorLists, new ResourceLocation("minecraft:village/snowy/houses"), "veggiesdelight:village/houses/snowy_depot_house", 5);
-        VillageStructures.addBuildingToPool(templatePools, processorLists, new ResourceLocation("minecraft:village/taiga/houses"), "veggiesdelight:village/houses/taiga_depot_house", 5);
-        VillageStructures.addBuildingToPool(templatePools, processorLists, new ResourceLocation("minecraft:village/savanna/houses"), "veggiesdelight:village/houses/savanna_depot_house", 4);
-        VillageStructures.addBuildingToPool(templatePools, processorLists, new ResourceLocation("minecraft:village/desert/houses"), "veggiesdelight:village/houses/desert_depot_house", 4);
+        // Vanilla Villages
+        if (Configuration.GENERATE_VANILLA_VS.get()) {
+            // Plains Village
+            VillageStructures.addBuildingToPool(templatePools, processorLists,
+                    new ResourceLocation("minecraft:village/plains/houses"),
+                    "veggiesdelight:village/houses/plains_depot_house", 4);
+            // Snowy Village
+            VillageStructures.addBuildingToPool(templatePools, processorLists,
+                    new ResourceLocation("minecraft:village/snowy/houses"),
+                    "veggiesdelight:village/houses/snowy_depot_house", 5);
+            // Taiga Village
+            VillageStructures.addBuildingToPool(templatePools, processorLists,
+                    new ResourceLocation("minecraft:village/taiga/houses"),
+                    "veggiesdelight:village/houses/taiga_depot_house", 5);
+            // Savanna Village
+            VillageStructures.addBuildingToPool(templatePools, processorLists,
+                    new ResourceLocation("minecraft:village/savanna/houses"),
+                    "veggiesdelight:village/houses/savanna_depot_house", 4);
+            // Desert Village
+            VillageStructures.addBuildingToPool(templatePools, processorLists,
+                    new ResourceLocation("minecraft:village/desert/houses"),
+                    "veggiesdelight:village/houses/desert_depot_house", 4);
+        }
+        // Atmospheric Villages Compat
         if (ModList.get().isLoaded("atmospheric")) {
-            VillageStructures.addBuildingToPool(
-                    templatePools, processorLists,
-                    new ResourceLocation("atmospheric:village/scrubland/houses"),
-                    "veggiesdelight:village/houses/scrubland_depot_house", 4);
+            if (Configuration.GENERATE_COMPAT_ATMOSPHERIC_VS.get()) {
+                // Scrubland Village
+                VillageStructures.addBuildingToPool(
+                        templatePools, processorLists,
+                        new ResourceLocation("atmospheric:village/scrubland/houses"),
+                        "veggiesdelight:village/houses/scrubland_depot_house", 4);
+            }
+        }
+        // Nature's Spirit Villages Compat
+        if (ModList.get().isLoaded("natures_spirit")) {
+            if (Configuration.GENERATE_COMPAT_NATURESPIRIT_VS.get()) {
+                // Adobe Village
+                VillageStructures.addBuildingToPool(
+                        templatePools, processorLists,
+                        new ResourceLocation("natures_spirit:village/adobe/adobe_houses_layer_1_back"),
+                        "veggiesdelight:village/houses/adobe_depot_house", 40);
+                // Coconut Village
+                VillageStructures.addBuildingToPool(
+                        templatePools, processorLists,
+                        new ResourceLocation("natures_spirit:village/coconut/houses"),
+                        "veggiesdelight:village/houses/coconut_depot_house", 3);
+                // Cypress Village
+                VillageStructures.addBuildingToPool(
+                        templatePools, processorLists,
+                        new ResourceLocation("natures_spirit:village/cypress/houses"),
+                        "veggiesdelight:village/houses/cypress_depot_house", 4);
+                // Wisteria Village
+                VillageStructures.addBuildingToPool(
+                        templatePools, processorLists,
+                        new ResourceLocation("natures_spirit:village/wisteria/houses"),
+                        "veggiesdelight:village/houses/wisteria_depot_house", 4);
+            }
         }
     }
 
